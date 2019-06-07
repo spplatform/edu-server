49498d2fe4ede805f9715b215ac42ea4797ddb29
Ilya Kaznacheev <ilyakaznacheev@oO.local>
2019-06-07 22:44:38 +0300

    Update some heroku configs

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/spplatform/edu-server
 
 go 1.12
+
+require github.com/gorilla/mux v1.7.2
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-import "os"
+import (
+	"os"
+
+	"github.com/spplatform/edu-server/internal/server"
+)
 
 func main() {
 	host := os.Getenv("HOST")
 	port := os.Getenv("PORT")
-	Serve(host, port)
+	server.Serve(host, port)
 }
